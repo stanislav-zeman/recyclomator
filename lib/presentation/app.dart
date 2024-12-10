9c1bb18a9b2ee37e8f84f98abb748d62e453514e
Stanislav Zeman <zeman@standa.dev>
2024-12-10 14:44:47 +0100

    refactor: rename files to match classes

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recyclomator/presentation/pages/homepage.dart';
-import 'package:recyclomator/presentation/theme/provider.dart';
+import 'package:recyclomator/presentation/theme/theme_provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
