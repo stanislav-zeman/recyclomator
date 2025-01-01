db9fff3de82a622a82e22e2af348ec20544e2b64
Machala, Oldřich <oldrich.machala@thermofisher.com>
2025-01-01 17:01:00 +0100

    feat: change the list style

--- lib/presentation/widgets/offers/offer_list.dart
+++ lib/presentation/widgets/offers/offer_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 import '../../../domain/entities/offer.dart';
 import '../../../domain/value_objects/item.dart';
@@ -19,16 +21,26 @@ class OfferList extends StatelessWidget {
       separatorBuilder: (_, __) => Divider(),
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
-          title: Text('Offer: ${offers[index].offerDate}'),
-          subtitle: Column(
+            title: Text('Offer: ${DateFormat('yyyy-MM-dd hh:mm:ss').format(offers[index].offerDate)}'),
+            subtitle: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('Pet bottles: ${_getNumberOfBottles(ItemType.pet, index)}'),
-              Text(
-                'Glass bottles: ${_getNumberOfBottles(ItemType.glass, index)}',
+              Row(
+              children: <Widget>[
+                Icon(FontAwesomeIcons.bottleWater),
+                SizedBox(width: 5),
+                Text(': ${_getNumberOfBottles(ItemType.pet, index)}'),
+              ],
+              ),
+              Row(
+              children: <Widget>[
+                Icon(FontAwesomeIcons.beerMugEmpty),
+                SizedBox(width: 5),
+                Text('Glass bottles: ${_getNumberOfBottles(ItemType.glass, index)}'),
+              ],
               ),
             ],
-          ),
+            ),
           trailing: Icon(Icons.arrow_forward_ios),
           onTap: () => Navigator.of(context).push(
             MaterialPageRoute<void>(
